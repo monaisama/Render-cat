394b0e091963314c59b527559c39ee03fd0e9686
kiritoren(任志豪) <kiritoren@tencent.com>
2023-11-24 16:52:10 +0800

    rotate camera and lookat zero-point

--- project-cat/src/main.cpp
+++ project-cat/src/main.cpp
@@ -92,7 +92,9 @@ int main()
 
         glClearColor(0.f, 0.f, 0.f, 1.f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        KRender render(mainCamera);
+
+        KVec3f newLoc {10 * sinf(glfwGetTime()), 10 * cosf(glfwGetTime()), 10};
+        KRender render(mainCamera.LookAt(newLoc, KVec3f::zero));
         
         // glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
         // example.Render(render);
